88d133a97cd11269a843ac087139352a85e63bf2
rossmcf <ross@rossmcf.com>
2016-06-03 09:54:21 +0100

    Add mock work item, for testing.

--- mockWorkItem.go
+++ mockWorkItem.go
@@ -8,14 +8,14 @@ const (
 // MockWorkItem is a simple work item for use in tests.
 type MockWorkItem struct {
 	content string
-	status  string
+	Status  string
 }
 
 // NewMockWorkItem returns a new mock work item with the given content.
-func NewMockWorkItem(content string) WorkItem {
+func NewMockWorkItem(content string) *MockWorkItem {
 	return &MockWorkItem{
 		content: content,
-		status:  fresh,
+		Status:  fresh,
 	}
 }
 
@@ -27,25 +27,25 @@ func (m *MockWorkItem) Content() (string, error) {
 // Start indicates that work is starting on the item.
 // Implementers should make sure that other workers cannot access the item.
 func (m *MockWorkItem) Start() {
-	m.status = processing
+	m.Status = processing
 }
 
 // Complete marks a work item as complete.
 // Further processing must be prevented.
 // The item may be retained for archival purposes.
 func (m *MockWorkItem) Complete(msg string) {
-	m.status = completed
+	m.Status = completed
 }
 
 // Fail indicates that work has not been successful, but the error may be recoverable.
 // The item should be retained for retries, and the number of retries should be recorded if necessary.
 func (m *MockWorkItem) Fail(msg string) {
-	m.status = failed
+	m.Status = failed
 }
 
 // Terminate indicates that work has not been successful, and that no further work can be done with the item.
 // Further processing must be prevented.
 // The item should be retained for review.
 func (m *MockWorkItem) Terminate(msg string) {
-	m.status = terminated
+	m.Status = terminated
 }
